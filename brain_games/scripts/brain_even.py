c1e3bacb79490219a842fef856eab6a40d6bce69
Monatov Vasiliy <moonman3000@yandex.ru>
2024-05-27 09:25:13 +0300

    change brain-even and brain-games add ascinema in README

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -5,8 +5,9 @@ import prompt
 import random
 from brain_games.welcome import welcome_user
 
+
 def even_or_no():
-    welcome_user()
+    name = welcome_user()
     print('Answer "yes" if the number is even, otherwise answer "no".')
     win = 0
     while win <= 2:
@@ -19,13 +20,13 @@ def even_or_no():
             result = "no"
         if result == answer:
             if win == 2:
-                print ('You win !!!')
+                print ('Congratulations, ' + name + '!')
                 break
             else:
                 print ("Correct!")
                 win += 1
         else:
-             print (f"""'{answer}' is wrong answer ;(. Correct answer was '{result}'.Let's try again, Bill!""")
+             print (f"""'{answer}' is wrong answer ;(. Correct answer was '{result}'.Let's try again, {name}!""")
              break
 
 
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,15 +1,8 @@
-#!/usr/bin/env python3
-
 from brain_games.cli import welcome_user
 
 
-def hello():
-    welcome_user()
-
-
 def main():
-    hello()
-
+    welcome_user()
 
 if __name__ == "__main__":
     main()
